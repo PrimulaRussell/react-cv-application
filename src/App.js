7b3616ebc756f526c6426cc8bf8496ccca22e597
primula <primularussell@gmail.com>
2021-02-11 12:39:20 -0700

    creater header file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import './App.css';
+import React from 'react';
+import './styles/App.css';
+import Header from './components/Header.js';
+import './styles/Header.css';
+
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const Header = () => {
+    return <h1 className='Header'>Create a CV!</h1>
+}
+
+export default Header;
