caec43761ee2f31e491b4c923a11ad30e2e0bd59
primula <primularussell@gmail.com>
2021-02-11 16:26:28 -0700

    Created editable mode, added General info component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './styles/App.css';
-import Header from './components/Header.js';
+import Header from './components/Header';
+import General from './components/General';
 
 document.body.style.backgroundColor = "#ccc";
 
-function App() {
-  return (
+class App extends Component{
+
+  state = {
+    value: "Text here",
+    isInEditMode: false
+  }
+
+  changeEditMode = () => {
+    this.setState({
+      isInEditMode: !this.state.isInEditMode
+    })
+  }
+  
+  renderEditView = () => {
+    return <div>
+        <input type="text"
+        defaultValue="Text goes here"
+        >
+        </input>
+      </div>
+  }
+
+  renderDefaultView = () => {
+  return <div onDoubleClick={this.changeEditMode}>
+  {this.state.value}
+  </div>
+  }
+
+  userInputArea = () => {
+    return this.state.isInEditMode ?
+    this.renderEditView
+    :
+    this.renderDefaultView
+
+  }
+  render(){
+  return(
     <div className="App">
       <Header></Header>
+      <General></General>
+      
     </div>
-  );
+  )};
 }
 
 export default App;
---
+++ src/components/General.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const General = () => {
+    return <div>
+    <h2>Name:</h2>
+    <h2>Email:</h2>
+    <h2>Phone Number:</h2>
+    </div>
+}
+
+export default General;
