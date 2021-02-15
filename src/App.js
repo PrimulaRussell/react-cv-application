dc9ec4c05caecd2892ca21bbc936539de28c6b98
primula <primularussell@gmail.com>
2021-02-14 22:12:34 -0700

    Styled General info

--- src/App.js
+++ src/App.js
@@ -19,8 +19,23 @@ class App extends Component{
   return(
     <div className="App">
     <Header></Header>
+    <div className="Box">
+    <div className="General">
+    <div className="generalName">
     <EditableField 
-    value={"Name"} />
+    value={"First Name"} />
+    <EditableField 
+    value={"Last Name"} />
+    </div>
+    <div className="generalContact">
+    <EditableField 
+    value={"Email"} />
+    <EditableField 
+    value={"Phone Number"} />
+    </div>
+    </div>
+    <h1>Education</h1>
+    </div>
     </div>
   )}
 }
